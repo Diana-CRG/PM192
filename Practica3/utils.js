ab7d1ee81fdfe802788cc7c458fe499e5e8d21b9
Diana-RG <144174071+Diana-CRG@users.noreply.github.com>
2025-06-04 23:47:35 -0600

    P3_ej_b

--- Practica3/utils.js
+++ Practica3/utils.js
@@ -1,3 +1,17 @@
 export function restar(a, b) {
   return a - b;
 }
+
+
+function verificarUsuario(usuario) {
+  
+}
+
+// Pruebas
+verificarUsuario("admin")
+  .then(res => console.log(res))
+  .catch(error => console.log(error));
+
+verificarUsuario("carolina")
+  .then(res => console.log(res))
+  .catch(error => console.log(error));
