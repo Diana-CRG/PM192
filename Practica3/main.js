cc22b47d89ea7f0b05cd92f1408cc073a9de6df0
Diana-RG <144174071+Diana-CRG@users.noreply.github.com>
2025-06-04 23:40:23 -0600

    P3_ej_a

---
+++ Practica3/main.js
@@ -0,0 +1,6 @@
+import { restar } from './utils.js';
+
+console.log("Pruebas de la función restar:");
+console.log("10 - 5 =", restar(10, 5));
+console.log("7 - 3 =", restar(7, 3));
+console.log("100 - 50 =", restar(100, 50));
