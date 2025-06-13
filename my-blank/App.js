6844e1d40b5a27617178d082112497cfa40cc045
Diana-RG <144174071+Diana-CRG@users.noreply.github.com>
2025-06-12 18:10:11 -0600

    Practica_5_Terminada

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,15 +1,32 @@
+/* zona 1, aqui van importaciones  */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
+import React, {useState} from 'react';
 
+const Texto=()=>{
+  const [contenido, setContenido]=useState('hola mundo React')
+  const actualizarTexto=()=>{setContenido('Estado actualizado')}
+  return(<Text onPress={actualizarTexto}>{contenido}</Text>) 
+}
+
+/* zona 2 , el main*/
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+        <Texto></Texto>
+        <Texto></Texto>
+        <Texto></Texto>
+        
+      <Button title='Presioname!'></Button>
+      
     </View>
   );
 }
 
+
+/* zona 3, estilos */ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
