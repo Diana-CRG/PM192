eb10ce86b2af5b0da438b83db03ead73ae99db67
Diana-RG <144174071+Diana-CRG@users.noreply.github.com>
2025-06-12 19:28:34 -0600

    pract_6_t

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,47 +1,44 @@
 /* zona 1, aqui van importaciones  */
 import { StatusBar } from 'expo-status-bar';
-import { Children } from 'react';
-import { StyleSheet, Text, View,Button } from 'react-native';
-import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
 
-const Texto=({style})=>{ //pasamo el parametro style 
-  const[contenido, setContenido]=useState('Hola mundo React')
-  const actualizarTexto=()=>{setContenido('Estado actualizado')}
-  return(
-        <Text style={[styles.text, style]} onPress={actualizarTexto}>{contenido}</Text>) //lo definimos en el componente padre 
-}
+/* Componente personalizado */
+const Texto = () => {
+  const [contenido, setContenido] = useState('hola mundo React');
+  const actualizarTexto = () => {
+    setContenido('estado actualizado');
+  };
+  return (
+    <Text style={styles.text} onPress={actualizarTexto}>
+      {contenido}
+    </Text>
+  );
+};
 
-/* zona 2 , el main*/
+/* zona 2 , el main */
 export default function App() {
   return (
     <View style={styles.container}>
-      <Texto style={styles.blue}>  </Texto>
-      <Texto style={styles.orange}> </Texto>
-      <Texto style={styles.pink}>  </Texto>
-      <Button title='Presioname!'></Button>
+      <Texto />
+      <Texto />
+      <Texto />
+      <Button title="Presioname!" onPress={() => {}} />
       <StatusBar style="auto" />
     </View>
   );
 }
 
-
-/* zona 3, estilos */ 
+/* zona 3, estilos */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'base-line',
+    alignItems: 'center',
     justifyContent: 'center',
-    flexDirection: 'column',
-  },
-text:{
-  color:'white',
-  fontSize:25,
-  height:100, /*de lo que se tenga disponible*/
-  /*width:100, nos va a crear el alto y ancho para cada texto*/
-
-},
-red:{backgroundColor:'blue'},
-green:{backgroundColor:'orange'},// se creo una clase para que sea de diferente como cada texto 
-blue:{backgroundColor:'pink'}, //agregamos el flex en cada clase 
+  },
+  text: {
+    color: 'red',
+    fontSize: 25,
+  },
 });
