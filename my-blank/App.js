3001a66409dad9d9e54ab4a64f210bdef5459859
Diana-RG <144174071+Diana-CRG@users.noreply.github.com>
2025-06-12 18:36:50 -0600

    Pract_6_Terminada

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,26 +1,25 @@
 /* zona 1, aqui van importaciones  */
 import { StatusBar } from 'expo-status-bar';
+import { Children } from 'react';
 import { StyleSheet, Text, View,Button } from 'react-native';
 import React, {useState} from 'react';
 
-const Texto=()=>{
-  const [contenido, setContenido]=useState('hola mundo React')
+const Texto=({style})=>{ //pasamo el parametro style 
+  const[contenido, setContenido]=useState('Hola mundo React')
   const actualizarTexto=()=>{setContenido('Estado actualizado')}
-  return(<Text onPress={actualizarTexto}>{contenido}</Text>) 
+  return(
+        <Text style={[styles.text, style]} onPress={actualizarTexto}>{contenido}</Text>) //lo definimos en el componente padre 
 }
 
 /* zona 2 , el main*/
 export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
-
-        <Texto></Texto>
-        <Texto></Texto>
-        <Texto></Texto>
-        
+      <Texto style={styles.blue}>  </Texto>
+      <Texto style={styles.orange}> </Texto>
+      <Texto style={styles.pink}>  </Texto>
       <Button title='Presioname!'></Button>
-      
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -31,7 +30,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'base-line',
     justifyContent: 'center',
-  },
+    flexDirection: 'column',
+  },
+text:{
+  color:'white',
+  fontSize:25,
+  height:100, /*de lo que se tenga disponible*/
+  /*width:100, nos va a crear el alto y ancho para cada texto*/
+
+},
+red:{backgroundColor:'blue'},
+green:{backgroundColor:'orange'},// se creo una clase para que sea de diferente como cada texto 
+blue:{backgroundColor:'pink'}, //agregamos el flex en cada clase 
 });
